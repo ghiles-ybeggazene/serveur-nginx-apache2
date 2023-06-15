183145c6bafa92c093f5f01f012debefd09c4e17
ghiles-ybeggazene <45173260+ghiles-ybeggazene@users.noreply.github.com>
2023-06-15 11:58:26 +0200

    Update React.js

--- React.js
+++ React.js
@@ -1,1 +1,21 @@
+npx create-react-app my-project
+
+
+  npm start
+    Starts the development server.
+
+  npm run build
+    Bundles the app into static files for production.
+
+  npm test
+    Starts the test runner.
+
+  npm run eject
+    Removes this tool and copies build dependencies, configuration files
+    and scripts into the app directory. If you do this, you can’t go back!
+
+We suggest that you begin by typing:
+
+  cd integration-project
+  npm start
 
